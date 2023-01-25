ffba53a4ca804bf6320f231813472f2013c520df
Manas Duishoev <manasduishoev@gmail.com>
2023-01-25 15:53:03 +0600

    Auto Layout profile page

--- iosApp/ViewController.swift
+++ iosApp/ViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        avatarImage.layer.borderWidth = 3.0
+        avatarImage.layer.borderColor = UIColor.lightGray.cgColor
         // Do any additional setup after loading the view.
     }
 
